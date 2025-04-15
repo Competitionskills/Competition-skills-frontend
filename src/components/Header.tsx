87862e798722b7994950e94e5c1b52c6e89fa606
zeekgelani <zeekgelani7@gmail.com>
2025-04-15 18:19:19 +0500

    Update Header.tsx
    changes Competition button to Dashboard button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,7 +16,7 @@ const Header = () => {
             <div className="hidden md:flex space-x-6">
               <Link to="/dashboard" className="flex items-center text-blue-600 font-bold hover:text-blue-700 transition-colors ">
                 <Trophy className="w-5 h-5 ml-2 mr-1" />
-                Competitions
+                Dashboard
               </Link>
               <Link to="/leaderboard" className="flex items-center text-gray-600 hover:text-blue-600 font-bold transition-colors">
                 <Crown className="w-5 h-5 mr-1" />
