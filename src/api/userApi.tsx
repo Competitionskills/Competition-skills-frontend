946da081382fcb6f55fb181c92cf6c98d36da2e5
zeekgelani <zeekgelani7@gmail.com>
2025-05-12 13:01:07 +0500

    Update userApi.tsx
    updated API for the Daily Login Reward

--- src/api/userApi.tsx
+++ src/api/userApi.tsx
@@ -1,4 +1,5 @@
 import api from "../helpers/axios";
+
 export const fetchUserProfile = async () => {
   const response = await api.get("/users/user");
   return response.data;
@@ -6,5 +7,15 @@ export const fetchUserProfile = async () => {
 
 export const buyPrestigeTicket = async () => {
   const response = await api.post("/tickets/buy-prestige-ticket");
+  return response.data;
+};
+
+export const getDailyLoginStatus = async () => {
+  const response = await api.get("/users/daily-login/status");
+  return response.data;
+};
+
+export const claimDailyReward = async () => {
+  const response = await api.post("/users/daily-login/claim");
   return response.data;
 };
